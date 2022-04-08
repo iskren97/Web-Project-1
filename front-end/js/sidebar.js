3d43d49da05e762f8289f5e8417a5bcaed9f9bde
Georgi Georgiev <gosho904@gmail.com>
2022-04-08 23:21:53 +0300

    Integrated sidebar opening/closing animation

---
+++ front-end/js/sidebar.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+
+
+btn.addEventListener("click",function() {
+   sidebar.classList.toggle("active")
+})
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log("READY")
+
+
+
+})
