812158bf9fe6999243da6d2f2a5d71213d5840a7
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-10 22:24:19 +0300

    removed 'k'

--- front-end/js/header-border.js
+++ front-end/js/header-border.js
@@ -2,8 +2,6 @@ const border = document.querySelector('.border-bottom');
 const body = document.querySelector('.main');
 
 body.addEventListener('scroll', () => {
-  console.log(body.scrollTop === 0);
-
   if (body.scrollTop !== 0) border.style.opacity = 0;
   else border.style.opacity = 1;
 });
