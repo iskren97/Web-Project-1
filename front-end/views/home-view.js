19f3a473083f18beb233a8a58daca020d0ee5b82
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-13 15:38:36 +0300

    Trending manipulation

--- front-end/views/home-view.js
+++ front-end/views/home-view.js
@@ -1,6 +1,5 @@
-
-export const homeView = () =>{
-  const main = document.querySelector("main")
+export const homeView = () => {
+  const main = document.querySelector('main');
 
   main.innerHTML = `
   <h1 class="gif-category">Editors picks:</h1>
@@ -23,13 +22,6 @@ export const homeView = () =>{
 
             <div class="gif-preview">
               <img class="thumbnail" src="icons/giphy.gif" />
-              <button class="like-btn">
-                <i class="fa fa-fw fa-heart"></i>
-              </button>
-            </div>
-
-            <div class="gif-preview">
-              <img class="thumbnail" src="icons/giphy.gif" alt="" />
               <button class="like-btn">
                 <i class="fa fa-fw fa-heart"></i>
               </button>
@@ -167,16 +159,15 @@ export const homeView = () =>{
               </button>
             </div>
           </section>
-  `
+  `;
 
   const trendingContainer = document.querySelector('#trending');
   const getTrending = async () => {
     const resp1 = await fetch(
-      'https://api.giphy.com/v1/gifs/trending?api_key=L6yFCUcFk8wlKFtQK3IemTQQd7JLiHv5&limit=21&rating=g'
+      'https://api.giphy.com/v1/gifs/trending?api_key=L6yFCUcFk8wlKFtQK3IemTQQd7JLiHv5&limit=14&rating=g'
     );
-  
+
     const data1 = await resp1.json();
-  
 
     data1.data.map((o) => {
       const html = `
@@ -187,25 +178,21 @@ export const homeView = () =>{
       </button>
       </div>
       `;
-  
+
       trendingContainer.insertAdjacentHTML('beforeend', html);
-  
-  
-      
     });
-  
-  
+
     const gifThumbnails = document.querySelectorAll('.gif-preview');
-  
+
     Array.from(gifThumbnails).forEach((el) => {
       const likeBtn = el.children[1];
-    
+
       el.addEventListener('mouseover', () => {
         if (likeBtn) {
           likeBtn.style.opacity = 1;
         }
       });
-    
+
       el.addEventListener('mouseout', () => {
         if (likeBtn) {
           likeBtn.style.opacity = 0;
@@ -214,5 +201,4 @@ export const homeView = () =>{
     });
   };
   getTrending();
-
-}
+};
