3644ed903679a550df9f830e25b65723fb5bad3e
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-14 20:02:12 +0300

    Implemented editors picks

--- front-end/views/single-gif-view.js
+++ front-end/views/single-gif-view.js
@@ -1,19 +1,18 @@
-
-
-export const generateView =(gif)=>{
-
+export const generateView = (gif) => {
   const username = gif.username || 'No username';
+  const title = gif.title || 'pixel gif';
+
   return `
   <div class="gif-preview">
   <img class="thumbnail" src="${gif.images.original.url}" alt="" />
   <div class="thumbnail-row">
   <p class="gif-info" style="margin-top: 5px">Author: ${username} </p>
   <p class="gif-info">Uploaded on: ${gif.import_datetime} </p>
-  <p class="gif-info">Title: ${gif.title} </p>
+  <p class="gif-info">Title: ${title} </p>
     <button class="like-btn">Like
       <i class="fa fa-fw fa-heart"></i>
     </button>
     </div>
   </div>
-  `
-}
+  `;
+};
