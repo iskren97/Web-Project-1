405a5bf58d93d232796529651dcb640778a4b40b
yoannary <ioannaiordanova0150@gmail.com>
2022-04-15 11:33:19 +0300

    Add id to single-gif-view

--- front-end/views/single-gif-view.js
+++ front-end/views/single-gif-view.js
@@ -3,7 +3,7 @@ export const generateView = (gif) => {
   const title = gif.title || 'pixel gif';
 
   return `
-  <div class="gif-preview">
+  <div class="gif-preview" data-gif-id="${gif.id}">
   <img class="thumbnail" src="${gif.images.original.url}" alt="" />
   <div class="thumbnail-row">
   <p class="gif-info" style="margin: 0.5em;overflow: clip;">Author: ${username} </p>
