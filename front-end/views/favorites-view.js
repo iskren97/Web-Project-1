e9c39750290a699a8b984e919db86ea729989207
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-16 19:00:13 +0300

    Performed tiny fix

--- front-end/views/favorites-view.js
+++ front-end/views/favorites-view.js
@@ -2,40 +2,39 @@ import { q } from '../helpers/helpers.js';
 import { getFavorites } from '../helpers/favorites.js';
 import { generateView } from './single-gif-view.js';
 import { getThumbnails } from '../helpers/thumbnails.js';
-export const favoritesView = async() =>{
-  const main = q("main")
-  main.innerHTML = "";
-  
+export const favoritesView = async () => {
+  const main = q('main');
+  main.innerHTML = '';
+
   const arrOfFavorite = getFavorites();
 
   const containerTitle = document.createElement('h1');
   containerTitle.innerHTML = 'Favorite GIFs:';
-  containerTitle.className = "gif-category";
-  
-  const container = document.createElement("section");
-  container.className = "gif-grid";
+  containerTitle.className = 'gif-category';
+
+  const container = document.createElement('section');
   main.appendChild(containerTitle);
   main.appendChild(container);
-  
-  if(arrOfFavorite.length === 0) {
-    container.innerHTML = '<p class="gif-category">Add some GiFs to favorites to see them here!</p>'
+
+  if (arrOfFavorite.length === 0) {
+    container.innerHTML =
+      '<p class="gif-category">Add some GiFs to favorites to see them here!</p>';
   } else {
+    container.className = 'gif-grid';
+
     const favorite = arrOfFavorite.toString();
 
     // use get gifs by id endpoint for all of the uploads
     const response = await fetch(
       `https://api.giphy.com/v1/gifs?api_key=L6yFCUcFk8wlKFtQK3IemTQQd7JLiHv5&ids=${favorite}`
-      );
-    const resultData = await response.json(); 
-     
+    );
+    const resultData = await response.json();
+
     resultData.data.map((o) => {
       const html = generateView(o);
       container.insertAdjacentHTML('beforeend', html);
     });
-    
+
     getThumbnails();
   }
-
-
-  
-}
+};
