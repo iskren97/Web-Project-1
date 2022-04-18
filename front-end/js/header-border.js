a7a4a4fbd14dba4f09e7aadaa9604e09d62a198c
Iskren Gyorev <iskrengyorev@gmail.com>
2022-04-18 14:04:42 +0300

    Code refactoring

--- front-end/js/header-border.js
+++ front-end/js/header-border.js
@@ -1,7 +1,9 @@
 const border = document.querySelector('.border-bottom');
 const body = document.querySelector('.main');
 
-body.addEventListener('scroll', () => {
-  if (body.scrollTop !== 0) border.style.opacity = 0;
-  else border.style.opacity = 1;
-});
+export const hideBorder = () => {
+  body.addEventListener('scroll', () => {
+    if (body.scrollTop !== 0) border.style.opacity = 0;
+    else border.style.opacity = 1;
+  });
+};
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,7 +1,8 @@
 import { homeView } from '../views/home-view.js';
 import { searchView } from '../views/search-view.js';
+import { hideBorder } from './header-border.js';
 
 homeView();
 searchView();
-
+hideBorder();
 document.addEventListener('DOMContentLoaded', async () => {});
