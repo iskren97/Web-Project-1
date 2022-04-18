1dc5987ee01e85eb80dab26d60bf27b617be380a
Georgi Georgiev <gosho904@gmail.com>
2022-04-18 18:53:32 +0300

    Fix: fetch post request

--- front-end/views/upload-gif-view.js
+++ front-end/views/upload-gif-view.js
@@ -1,59 +1,62 @@
-import { q, create } from '../helpers/helpers.js';
-import { addUpload } from '../helpers/uploads.js';
+import { q, create } from "../helpers/helpers.js";
+import { addUpload } from "../helpers/uploads.js";
 
 /**
  * Generates the upload-gif view.
  */
 export const uploadView = () => {
-  const main = q('main');
-  main.innerHTML = '';
+  const main = q("main");
+  main.innerHTML = "";
 
-  const container = create('section');
-  container.className = 'gif-grid';
+  const container = create("section");
+  container.className = "gif-grid";
 
-  const containerTitle = create('h1');
-  containerTitle.innerHTML = 'Upload a GIF:';
-  containerTitle.className = 'gif-category';
+  const containerTitle = create("h1");
+  containerTitle.innerHTML = "Upload a GIF:";
+  containerTitle.className = "gif-category";
 
   main.appendChild(containerTitle);
 
-  const inputField = create('input');
-  inputField.type = 'file';
-  inputField.id = 'fileInput';
-  inputField.className = 'upload-input';
-  inputField.accept = '.gif';
+  const inputField = create("input");
+  inputField.type = "file";
+  inputField.id = "fileInput";
+  inputField.className = "upload-input";
+  inputField.accept = ".gif";
   main.appendChild(inputField);
 
-  const responseMsg = create('h1');
-  responseMsg.className = 'gif-category';
+  const responseMsg = create("h1");
+  responseMsg.className = "gif-category";
   main.appendChild(responseMsg);
 
-  const fileInput = q('#fileInput');
+  const fileInput = q("#fileInput");
 
-  const uploadFile = (file) => {
-    responseMsg.innerHTML = 'Uploading a GIF...';
-    if (file.type.match('image/gif')) {
-      const API_ENDPOINT = 'https://upload.giphy.com/v1/gifs?api_key=L6yFCUcFk8wlKFtQK3IemTQQd7JLiHv5';
+  const uploadFile = async (file) => {
+    if (file.type.match("image/gif")) {
+      responseMsg.innerHTML = "Uploading a GIF...";
+      const API_ENDPOINT =
+        "https://upload.giphy.com/v1/gifs?api_key=L6yFCUcFk8wlKFtQK3IemTQQd7JLiHv5";
 
-      const request = new XMLHttpRequest();
       const formData = new FormData();
+      formData.append("file", file);
 
-      request.open('POST', API_ENDPOINT, true);
-      request.onreadystatechange = () => {
-        if (request.readyState === 4 && request.status === 200) {
-          const response = JSON.parse(request.responseText);
-          addUpload(response.data.id);
-          responseMsg.innerHTML = 'File uploaded successfully!';
-        }
-      };
-      formData.append('file', file);
-      request.send(formData);
+      const req = await fetch(API_ENDPOINT, {
+        method: "POST",
+        body: formData,
+      });
+
+      const res = await req.json();
+      addUpload(res.data.id);
+
+
+      if (res.meta.status == 200) {
+        responseMsg.innerHTML = "File uploaded successfully!";
+      }
     } else {
-      responseMsg.innerHTML = 'Please upload a gif file!';
+      responseMsg.innerHTML = "Please upload a gif file!";
     }
   };
 
-  fileInput.addEventListener('change', (event) => {
+  fileInput.addEventListener("change", (event) => {
     const files = event.target.files;
     uploadFile(files[0]);
   });
